47a219323412781f48fc35cc3f4db84c79da6220
Ilya <2002marsic@gmail.com>
2022-10-21 21:36:38 +0000

    CI and tests running changed

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,6 +5,6 @@ set -e
 cd build
 ctest -VV
 
-#lcov -t "tests/tests_fib" -o coverage.info -c -d fib_lib/
+apt-get install lcov
 lcov -t "tests/tests_net" -o coverage.info -c -d net/
 genhtml -o report coverage.info
