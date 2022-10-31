d9dbf45fa6d6caf36628bfaa20d16fce017d4c5b
tanyagaKim <tatchuikova@gmail.com>
2022-10-31 15:15:04 +0000

    Add api

--- project/src/client/client.cpp
+++ project/src/client/client.cpp
@@ -1,7 +1,6 @@
 #include "client.hpp"
 
 namespace project {
-Client::Client() { std::cout << "Creating client" << std::endl; }
 
 int Client::connectToServer() {
   // TO_DO
--- project/src/client/client.hpp
+++ project/src/client/client.hpp
@@ -9,7 +9,7 @@ namespace project {
 struct Client {
   Client() {std::cout << "Create client" << std::endl;} 
   static int connectToServer();
-  ~CLient() {std::cout << "Delete client" << std::endl;} 
+  ~Client() {std::cout << "Delete client" << std::endl;} 
 };
 
 }  // namespace project
